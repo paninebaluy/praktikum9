c12c1541699294fb4b51d7072abae87e4a7d8d8b
Natalia Dolgushina <paninebaluy@gmail.com>
2020-07-29 18:41:08 +0300

    reversed sorting

--- scripts/cardlist.js
+++ scripts/cardlist.js
@@ -27,7 +27,7 @@ class CardList {
             };
             const anotherCard = this.newCard(cardParams, this.functions, this.myData);
             anotherCard.create();   
-            this.container.insertBefore(container.firstChild, anotherCard.card);                               
+            this.container.insertBefore(anotherCard.card, this.container.firstChild);                               
         }.bind(this)))
         .catch( (err) => console.log('Failed to render cards. Reason: ' + err) );
     }
